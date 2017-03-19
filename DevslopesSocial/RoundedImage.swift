0f4d9eaa687b94745e9304eac858aac43bff24e8
jthomson93 <james@thomsoncreative.co.uk>
2017-03-19 14:21:13 +0000

    Finished Feed UI

---
+++ DevslopesSocial/RoundedImage.swift
@@ -0,0 +1,23 @@
+//
+//  RoundedImage.swift
+//  DevslopesSocial
+//
+//  Created by James Thomson on 19/03/2017.
+//  Copyright © 2017 James Thomson. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedImage: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 22.5
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.shadowOpacity = 0.8
+    }
+
+}
