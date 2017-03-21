246a920c8c2f57861ed5b9b1934eb02ea5529ab9
jthomson93 <james@thomsoncreative.co.uk>
2017-03-21 23:19:01 +0000

    Added image cacheing
    This was added to improve image caching such that the app would run
    responsively.

--- DevslopesSocial/FeedVC.swift
+++ DevslopesSocial/FeedVC.swift
@@ -17,6 +17,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     
     var posts = [Post]()
     var imagePicker: UIImagePickerController!
+    static var imageCache: NSCache<NSString, UIImage> = NSCache()
 
     
     override func viewDidLoad() {
@@ -61,6 +62,9 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
         let post = posts[indexPath.row]
         
         if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            if let img = FeedVC.imageCache.object(forKey: post.imageUrl as NSString) {
+                cell.configureCell(post: post, img: img)
+            }
             cell.configureCell(post: post)
             return cell
         } else {
--- DevslopesSocial/PostCell.swift
+++ DevslopesSocial/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
     
@@ -23,9 +24,30 @@ class PostCell: UITableViewCell {
         // Initialization code
     }
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage? = nil) {
         self.post = post
         self.captionLbl.text = post.caption
         self.likesLbl.text = String(post.likes)
+        
+        if img != nil {
+            self.postImg.image = img
+        } else {
+            
+            let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+            ref.data(withMaxSize: 2 * 1024 * 1024, completion: { (data, error) in
+                if error != nil {
+                    print("JAMIE: Unable to download image from Firebase storage")
+                } else {
+                    print("JAMIE: Image has been successfully downloaded")
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.postImg.image = img
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                        }
+                    }
+                }
+                
+            })
+        }
     }
 }
